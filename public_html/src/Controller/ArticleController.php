826d3d1051e4d94d235acac8da88877fd4ebe2f4
Satyadarshin Perry <s.Dog@Satyadarshin.com>
2019-05-14 14:21:48 +0100

    Initial commit

---
+++ public_html/src/Controller/ArticleController.php
@@ -0,0 +1,12 @@
+<?php
+    namespace App\Controller;
+
+    use Symfony\Component\HttpFoundation\Response;
+
+    Class ArtcicleController {
+
+        public function index() {
+            return new esponse('<htm><body><h1>test</h1></body></html>');
+        }
+    }
+?>
